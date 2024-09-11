7d6a4afd9d79d80ddc750e9e6ca5a274fbbba85b
enjnks <yangzhou0314@outlook.com>
2024-09-12 02:07:32 +0800

    20240912_001

--- models/Koopa.py
+++ models/Koopa.py
@@ -3,25 +3,33 @@ import torch
 import torch.nn as nn
 
 
-
+#将时间序列分解为（周期性）时变（time-variant）和(非周期性)时不变（time-invariant）部分
 class FourierFilter(nn.Module):
     """
     Fourier Filter: to time-variant and time-invariant term
     """
-    def __init__(self, mask_spectrum):
+    def __init__(self, mask_spectrum): 
+        #参数mask_spectrum, 是一个布尔数组或者索引数组，用于指定在傅里叶变换后的频谱中哪些频率成分应该被保留，哪些应该被置零。
+        #在时不变成分的提取中，通常将低频成分保留，而将高频成分置零。
         super(FourierFilter, self).__init__()
         self.mask_spectrum = mask_spectrum
         
     def forward(self, x):
-        xf = torch.fft.rfft(x, dim=1)
-        mask = torch.ones_like(xf)
-        mask[:, self.mask_spectrum, :] = 0
-        x_var = torch.fft.irfft(xf*mask, dim=1)
-        x_inv = x - x_var
+        #函数接收一个输入x，这是一个三维张量，其形状为(B, L, C)，其中B是批大小，L是序列长度，C是特征数量。
+        xf = torch.fft.rfft(x, dim=1) #这个函数对输入x的每个序列（沿着维度1）进行快速傅里叶变换（FFT），将时域信号转换到频域。rfft是实数输入的FFT，它返回的是每个序列的傅里叶变换的振幅和相位信息。
+        mask = torch.ones_like(xf) #创建一个与xf形状相同的全一张量，用于构建掩码。
+        mask[:, self.mask_spectrum, :] = 0 #根据mask_spectrum参数，将掩码中的特定频率成分置零。在逆变换时，这些频率成分将不会被考虑，从而可以从原始信号中去除这些成分。
+        # 第一个:表示对所有批次（batch）进行操作。
+        # self.mask_spectrum表示在频谱的第二维（通常是频率维度）中选择特定的索引，这些索引对应于需要被置零的频率成分。
+        # 第三个:表示对所有特征进行操作。
+        x_var = torch.fft.irfft(xf*mask, dim=1) #使用修改后的频谱（原始频谱与掩码相乘）进行逆傅里叶变换，得到时变成分。这是因为在频域中置零的频率成分在时域中对应于原信号的时变部分。
+        #这是逆变换的结果，它包含了时变成分的时域表示。由于在频域中已经通过掩码去除了某些频率成分，x_var只包含了原始信号中对应于未被置零频率成分的部分。换句话说，x_var是原始信号中变化部分的表示，这些变化部分可能对应于信号中的周期性或非平稳成分。
+        #dim=1意味着对每个批次中的序列（即每个时间序列）单独进行逆变换。这是因为输入x的形状是(B, L, C)，其中B是批次大小，L是序列长度，C是特征数量。dim=1确保了对每个序列独立处理。
+        x_inv = x - x_var #计算时不变成分，即原始信号减去时变成分。
         
         return x_var, x_inv
     
-
+#多层感知机（Multilayer Perceptron）类，用于编码和解码序列数据的高维表示。它包含输入层、隐藏层和输出层，以及激活函数和dropout。
 class MLP(nn.Module):
     '''
     Multilayer perceptron to encode/decode high dimension representation of sequential data
@@ -33,35 +41,43 @@ class MLP(nn.Module):
                  hidden_layers=2, 
                  dropout=0.05,
                  activation='tanh'): 
+        #初始化
         super(MLP, self).__init__()
-        self.f_in = f_in
-        self.f_out = f_out
-        self.hidden_dim = hidden_dim
-        self.hidden_layers = hidden_layers
-        self.dropout = dropout
+        self.f_in = f_in #输入特征的维度
+        self.f_out = f_out #输出特征的维度
+        self.hidden_dim = hidden_dim #隐藏层神经元的数量，传参为128
+        self.hidden_layers = hidden_layers #隐藏层的数量，传参为2
+        self.dropout = dropout #为了防止过拟合，设置的dropout比率，传参为0.05。
+        #激活函数的类型，传参为'tanh'
         if activation == 'relu':
             self.activation = nn.ReLU()
         elif activation == 'tanh':
             self.activation = nn.Tanh()
         else:
-            raise NotImplementedError
+            raise NotImplementedError #如果是其他值，则抛出NotImplementedError异常，表示不支持的激活函数
 
+        #构建网络层
+        #layers列表用于存储网络中的所有层
+        #首先，创建一个nn.Linear层，将输入特征从f_in维度映射到hidden_dim维度，然后添加激活函数和dropout层。
         layers = [nn.Linear(self.f_in, self.hidden_dim), 
                   self.activation, nn.Dropout(self.dropout)]
+        #self.hidden_layers是构造函数中定义的隐藏层总数。减去2是因为第一个隐藏层已经在循环之前被添加到了layers列表中，而循环的最后一次迭代将添加最后一个隐藏层（如果存在）。例如，如果self.hidden_layers是2，那么这个循环不会执行，因为range(0)是空的；如果self.hidden_layers是3，循环将执行一次。
         for i in range(self.hidden_layers-2):
             layers += [nn.Linear(self.hidden_dim, self.hidden_dim),
                        self.activation, nn.Dropout(dropout)]
         
+        #最后，添加一个nn.Linear层，将最后一个隐藏层的特征从hidden_dim映射到输出特征的维度f_out。
         layers += [nn.Linear(hidden_dim, f_out)]
+        #使用nn.Sequential容器将所有层按顺序包装起来，这样可以通过简单地调用self.layers(x)来执行前向传播。
         self.layers = nn.Sequential(*layers)
 
     def forward(self, x):
         # x:     B x S x f_in
         # y:     B x S x f_out
-        y = self.layers(x)
-        return y
+        y = self.layers(x) #x：输入张量，形状为(B, S, f_in)，其中B是批大小，S是序列长度，f_in是输入特征的维度。
+        return y #y：输出张量，通过self.layers(x)计算得到，形状为(B, S, f_out)，表示经过多层感知机处理后的特征。
     
-
+#用于通过动态模态分解（DMD）迭代地找到线性系统的一步转换
 class KPLayer(nn.Module):
     """
     A demonstration of finding one step transition of linear system by DMD iteratively
@@ -99,7 +115,7 @@ class KPLayer(nn.Module):
         z_preds = torch.cat(z_preds, dim=1)
         return z_rec, z_preds
 
-
+#用于通过多步K近似来找到线性系统的库普曼转换
 class KPLayerApprox(nn.Module):
     """
     Find koopman transition of linear system by DMD with multistep K approximation
@@ -146,7 +162,7 @@ class KPLayerApprox(nn.Module):
 
         return z_rec, z_pred
     
-
+#用于利用滑动窗口内的局部变化来预测时间变化项的未来
 class TimeVarKP(nn.Module):
     """
     Koopman Predictor with DMD (analysitical solution of Koopman operator)
@@ -198,7 +214,7 @@ class TimeVarKP(nn.Module):
 
         return x_rec, x_pred
 
-
+#用于利用回顾和预测窗口快照来预测时间不变项的未来
 class TimeInvKP(nn.Module):
     """
     Koopman Predictor with learnable Koopman operator
@@ -232,7 +248,7 @@ class TimeInvKP(nn.Module):
 
         return res
 
-
+#主模型类，完整的库普曼预测模型
 class Model(nn.Module):
     '''
     Koopman Forecasting Model
@@ -306,3 +322,13 @@ class Model(nn.Module):
         res = forecast * std_enc + mean_enc
 
         return res
+    
+
+
+    #代码的主要流程如下：
+
+    # 输入时间序列数据x_enc，首先进行平稳化处理。
+    # 使用FourierFilter将序列分解为时变和时不变部分。
+    # 对于每个分解部分，分别使用TimeInvKP和TimeVarKP进行预测。
+    # 将预测结果相加，得到最终的预测序列。
+    # 对预测序列进行反平稳化处理，得到最终的预测结果。
